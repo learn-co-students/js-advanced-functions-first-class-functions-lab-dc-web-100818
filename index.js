e2adc7967e8184c86a297bff0af5591ae012870a
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-11-19 18:08:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+
+  return arr.slice(0,2)
+
+
+}
+
+const returnLastTwoDrivers = function (arr) {
+
+  return arr.slice(-2)
+
+
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier (multipler) {
+  return function farer(fare) {
+    return multipler * fare
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+function fetchSpecifiedDrivers (drivers, funcs) {
+  return  funcs(drivers)
+}
